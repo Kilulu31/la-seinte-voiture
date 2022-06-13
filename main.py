d4b9584de9b78d46ed71c33d4da9df77b310bc16
Kilulu31 <102540091+Kilulu31@users.noreply.github.com>
2022-06-13 18:19:14 +0200

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,52 +1,128 @@
-def on_received_number(receivedNumber):
-    global VITESSE
-    if receivedNumber == 0:
-        servos.P0.run(VITESSE)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        if (auto == 0) {
+            if (obstacl == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VITESSE)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+                basic.pause(10)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            }
+        }
+    }
+    if (receivedNumber == 1) {
+        if (auto == 0) {
+            if (obstacl == 0) {
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, VITESSE)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+                basic.pause(10)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            }
+        }
+    }
+    if (receivedNumber == 4) {
+        if (auto == 0) {
+            if (obstacl == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 70)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 70)
+                basic.pause(10)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            }
+        }
+    }
+    if (receivedNumber == 2) {
+        if (VITESSE > 100) {
+            VITESSE = VITESSE + 20
+        }
+    }
+    if (receivedNumber == 3) {
+        if (true) {
+            VITESSE = VITESSE - 20
+        }
+    }
+    if (receivedNumber == 4) {
+        if (auto == 0) {
+            auto = 1
+        } else {
+            auto = 0
+        }
+    }
+})
+input.onGesture(Gesture.LogoUp, function () {
+    while (input.isGesture(Gesture.LogoUp)) {
+        radio.sendNumber(4)
         basic.pause(10)
-        servos.P0.run(0)
-    if receivedNumber == 1:
-        servos.P1.run(VITESSE)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    while (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(3)
         basic.pause(10)
-        servos.P1.run(0)
-    if receivedNumber == 4:
-        servos.P0.run(-40)
-        servos.P1.run(-40)
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    while (input.isGesture(Gesture.TiltRight)) {
+        radio.sendNumber(1)
         basic.pause(10)
-        servos.P0.run(0)
-        servos.P1.run(0)
-    if receivedNumber == 2:
-        if VITESSE > 100:
-            VITESSE = VITESSE + 5
-    if receivedNumber == 3:
-        if True:
-            VITESSE = VITESSE - 10
-radio.on_received_number(on_received_number)
-
-def on_gesture_logo_up():
-    while input.is_gesture(Gesture.LOGO_UP):
-        radio.send_number(4)
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    while (input.isGesture(Gesture.TiltLeft)) {
+        radio.sendNumber(0)
         basic.pause(10)
-input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
-
-def on_gesture_tilt_right():
-    while input.is_gesture(Gesture.TILT_RIGHT):
-        radio.send_number(1)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    while (input.buttonIsPressed(Button.AB)) {
+        radio.sendNumber(4)
         basic.pause(10)
-input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
-
-def on_gesture_tilt_left():
-    while input.is_gesture(Gesture.TILT_LEFT):
-        radio.send_number(0)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
         basic.pause(10)
-input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
-
-def on_gesture_logo_down():
-    while input.is_gesture(Gesture.LOGO_DOWN):
-        radio.send_number(0)
-        radio.send_number(1)
+    }
+})
+input.onGesture(Gesture.LogoDown, function () {
+    while (input.isGesture(Gesture.LogoDown)) {
+        radio.sendNumber(0)
+        radio.sendNumber(1)
         basic.pause(10)
-input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
-
-VITESSE = 0
-radio.set_group(1)
-VITESSE = 30
+    }
+})
+let obstacl = 0
+let auto = 0
+let VITESSE = 0
+radio.setGroup(1)
+VITESSE = 150
+basic.forever(function () {
+    if (auto == 1) {
+        while (auto == 1) {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+            }
+        }
+    }
+    while (auto == 0) {
+        if (IR.IR_read() == 1) {
+            obstacl = 1
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 150)
+            basic.pause(1000)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+            obstacl = 0
+        }
+    }
+})
--- main.ts
+++ main.ts
@@ -1,29 +1,51 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
-        servos.P0.run(VITESSE)
-        basic.pause(10)
-        servos.P0.run(0)
+        if (auto == 0) {
+            if (obstacl == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VITESSE)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+                basic.pause(10)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            }
+        }
     }
     if (receivedNumber == 1) {
-        servos.P1.run(VITESSE)
-        basic.pause(10)
-        servos.P1.run(0)
+        if (auto == 0) {
+            if (obstacl == 0) {
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, VITESSE)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+                basic.pause(10)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            }
+        }
     }
     if (receivedNumber == 4) {
-        servos.P0.run(-40)
-        servos.P1.run(-40)
-        basic.pause(10)
-        servos.P0.run(0)
-        servos.P1.run(0)
+        if (auto == 0) {
+            if (obstacl == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 70)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 70)
+                basic.pause(10)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            }
+        }
     }
     if (receivedNumber == 2) {
         if (VITESSE > 100) {
-            VITESSE = VITESSE + 5
+            VITESSE = VITESSE + 20
         }
     }
     if (receivedNumber == 3) {
         if (true) {
-            VITESSE = VITESSE - 10
+            VITESSE = VITESSE - 20
+        }
+    }
+    if (receivedNumber == 4) {
+        if (auto == 0) {
+            auto = 1
+        } else {
+            auto = 0
         }
     }
 })
@@ -34,7 +56,10 @@ input.onGesture(Gesture.LogoUp, function () {
     }
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(3)
+    while (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(3)
+        basic.pause(10)
+    }
 })
 input.onGesture(Gesture.TiltRight, function () {
     while (input.isGesture(Gesture.TiltRight)) {
@@ -48,8 +73,17 @@ input.onGesture(Gesture.TiltLeft, function () {
         basic.pause(10)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    while (input.buttonIsPressed(Button.AB)) {
+        radio.sendNumber(4)
+        basic.pause(10)
+    }
+})
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
+    while (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+        basic.pause(10)
+    }
 })
 input.onGesture(Gesture.LogoDown, function () {
     while (input.isGesture(Gesture.LogoDown)) {
@@ -58,6 +92,38 @@ input.onGesture(Gesture.LogoDown, function () {
         basic.pause(10)
     }
 })
+let obstacl = 0
+let auto = 0
 let VITESSE = 0
 radio.setGroup(1)
-VITESSE = 30
+VITESSE = 150
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+basic.forever(function () {
+    if (auto == 1) {
+        while (auto == 1) {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+            }
+        }
+    }
+    while (auto == 0) {
+        if (IR.IR_read() == 1) {
+            obstacl = 1
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 150)
+            basic.pause(1000)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+            obstacl = 0
+        }
+    }
+})
